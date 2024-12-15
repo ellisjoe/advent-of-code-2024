f488d9304035c49dc8650d19d9fe3ec09a78ec11
Joe Ellis <ellis125@gmail.com>
2024-12-15 13:03:01 -0700

    Day 15 Part 2

--- lib/src/test/java/com/ellisjoe/aoc/Day15.java
+++ lib/src/test/java/com/ellisjoe/aoc/Day15.java
@@ -5,13 +5,17 @@ import com.ellisjoe.aoc.utils.Point;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class Day15 {
     @Test
     void part1() {
         List<String> lines = Utils.readLines(15, false);
 
-        Input input = parseInput(lines);
+        Input input = parseInput(lines, 1);
 
         List<Direction> directions = input.directions();
         Map map = input.map();
@@ -22,6 +26,28 @@ public class Day15 {
 
         long result = map.boxes()
                 .stream()
+                .map(Rect::min)
+                .mapToLong(p -> 100 * p.y() + p.x())
+                .sum();
+        System.out.println(result);
+    }
+
+    @Test
+    void part2() {
+        List<String> lines = Utils.readLines(15, false);
+
+        Input input = parseInput(lines, 2);
+
+        List<Direction> directions = input.directions();
+        Map map = input.map();
+
+        for (Direction dir : directions) {
+            map = stepMap(dir, map);
+        }
+
+        long result = map.boxes()
+                .stream()
+                .map(Rect::min)
                 .mapToLong(p -> 100 * p.y() + p.x())
                 .sum();
         System.out.println(result);
@@ -29,34 +55,49 @@ public class Day15 {
 
     private static Map stepMap(Direction dir, Map map) {
         Point robot = map.robot();
-        Set<Point> boxes = map.boxes();
-        Set<Point> walls = map.walls();
+        Set<Rect> boxesToCheck = new HashSet<>(map.boxes());
+        Set<Rect> walls = map.walls();
 
-        Point newLoc = robot.move(dir);
-        List<Point> boxesToMove = new ArrayList<>();
-        while (boxes.contains(newLoc)) {
-            boxesToMove.add(newLoc);
-            newLoc = newLoc.move(dir);
+        List<Point> locationsToPush = new LinkedList<>();
+        List<Rect> boxesToMove = new ArrayList<>();
+        locationsToPush.add(robot.move(dir));
+        while (!locationsToPush.isEmpty()) {
+            Point newLoc = locationsToPush.removeFirst();
+            Optional<Rect> maybeBox = boxesToCheck.stream().filter(r -> r.intersects(newLoc)).findFirst();
+            if (maybeBox.isPresent()) {
+                Rect box = maybeBox.get();
+                locationsToPush.addAll(box.move(dir).points());
+                boxesToCheck.remove(box);
+                boxesToMove.add(box);
+            }
+
+            Optional<Rect> maybeWall = walls.stream().filter(r -> r.intersects(newLoc)).findFirst();
+            if (maybeWall.isPresent()) {
+                return map;
+            }
         }
-        if (!walls.contains(newLoc)) {
-            robot = robot.move(dir);
-            boxes.removeAll(boxesToMove);
-            boxes.addAll(boxesToMove.stream().map(b -> b.move(dir)).toList());
-        }
+
+        robot = robot.move(dir);
+        HashSet<Rect> boxes = new HashSet<>(map.boxes());
+        boxes.removeAll(boxesToMove);
+        boxes.addAll(boxesToMove.stream().map(b -> b.move(dir)).toList());
         return new Map(walls, boxes, robot);
     }
 
     private static void print(Map map) {
-        long xMax = map.walls().stream().mapToLong(Point::x).max().orElseThrow();
-        long yMax = map.walls().stream().mapToLong(Point::y).max().orElseThrow();
+        Set<Point> wallPoints = map.walls().stream().flatMap(r -> r.points().stream()).collect(Collectors.toSet());
+        Set<Point> boxPoints = map.boxes().stream().flatMap(b -> b.points().stream()).collect(Collectors.toSet());
+
+        long xMax = wallPoints.stream().mapToLong(Point::x).max().orElseThrow();
+        long yMax = wallPoints.stream().mapToLong(Point::y).max().orElseThrow();
 
         for (int y = 0; y <= yMax; y++) {
             for (int x = 0; x <= xMax; x++) {
                 char c = '.';
                 Point p = new Point(x, y);
-                if (map.walls().contains(p)) {
+                if (wallPoints.contains(p)) {
                     c = '#';
-                } else if (map.boxes().contains(p)) {
+                } else if (boxPoints.contains(p)) {
                     c = 'O';
                 } else if (map.robot().equals(p)) {
                     c = '@';
@@ -67,7 +108,7 @@ public class Day15 {
         }
     }
 
-    private static Input parseInput(List<String> lines) {
+    private static Input parseInput(List<String> lines, long width) {
         boolean inMap = true;
         List<String> mapLines = new ArrayList<>();
         List<String> directionLines = new ArrayList<>();
@@ -81,17 +122,18 @@ public class Day15 {
             }
         }
 
-        Set<Point> boxes = new HashSet<>();
-        Set<Point> walls = new HashSet<>();
+        Set<Rect> boxes = new HashSet<>();
+        Set<Rect> walls = new HashSet<>();
         Point robot = null;
         for (int y = 0; y < mapLines.size(); y++) {
             String row = mapLines.get(y);
             for (int x = 0; x < row.length(); x++) {
                 switch (row.charAt(x)) {
-                    case '.' -> {}
-                    case '#' -> walls.add(new Point(x, y));
-                    case 'O' -> boxes.add(new Point(x, y));
-                    case '@' -> robot = new Point(x, y);
+                    case '.' -> {
+                    }
+                    case '#' -> walls.add(Rect.of(width, new Point(x * width, y)));
+                    case 'O' -> boxes.add(Rect.of(width, new Point(x * width, y)));
+                    case '@' -> robot = new Point(x * width, y);
                     default -> throw new IllegalStateException("Unexpected value: " + row.charAt(x));
                 }
             }
@@ -110,7 +152,34 @@ public class Day15 {
     }
 
 
-    record Map(Set<Point> walls, Set<Point> boxes, Point robot) {}
+    record Map(Set<Rect> walls, Set<Rect> boxes, Point robot) {
+    }
 
-    record Input(Map map, List<Direction> directions) {}
+    record Input(Map map, List<Direction> directions) {
+    }
+
+    record Rect(Set<Point> points) {
+        static Rect of(long width, Point point) {
+            Set<Point> points = LongStream.range(0, width)
+                    .mapToObj(offset -> new Point(point.x() + offset, point.y()))
+                    .collect(Collectors.toSet());
+            return new Rect(points);
+        }
+
+        static Rect of(Point... points) {
+            return new Rect(Set.of(points));
+        }
+
+        Rect move(Direction dir) {
+            return new Rect(points.stream().map(p -> p.move(dir)).collect(Collectors.toSet()));
+        }
+
+        boolean intersects(Point p) {
+            return points.contains(p);
+        }
+
+        Point min() {
+            return points.stream().min(Comparator.comparing(Point::x)).orElseThrow();
+        }
+    }
 }
